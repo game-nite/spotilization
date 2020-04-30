413b5034da0ce8dbc19749edd543fdd2f31e9a83
Jeff Zhan <jeffzhan2008@gmail.com>
2020-04-30 13:55:06 -0400

    pull given a user's id their top 50 artists

--- test scripts/spotifyapitesting.py
+++ test scripts/spotifyapitesting.py
@@ -8,6 +8,24 @@ class SpotifyAPIs:
 
 	def __init__(self): 
 		self.user_id = spotify_user_id
-    	self.spotify_token = spotify_token_1
-	
-	# def topArtists(self): 
+		self.spotify_token = spotify_token_1
+		
+	def topArtists(self): 
+		q = "https://api.spotify.com/v1/me/top/{}?time_range={}&limit={}".format("artists", "long_term", "50")
+		r = requests.get(
+			q, 
+			headers = {
+				"Content-Type": "application/json",
+				"Authorization": "Bearer {}".format(self.spotify_token),
+			}
+		)
+		response_json = r.json() 
+		topArtists = [] 
+		for item in response_json['items']: 
+			if item['name']: 
+				topArtists.append(item['name'])
+		return topArtists
+
+if __name__ == '__main__':
+	cp = SpotifyAPIs() 
+	print(cp.topArtists())
